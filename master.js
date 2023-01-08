f3adbaeb93b3f63bb6221982435fb3d32073f757
abdelrhman ashraf <mido59478@gmail.com>
2023-01-08 10:15:46 +0200

    second update

--- master.js
+++ master.js
@@ -12,7 +12,7 @@ btnChange.forEach(btn => {
             imgMain.src = "https://i.gifer.com/5J46.gif";
         } else {
             document.title = "| Sign Up Content |"
-            imgMain.src = "https://static.vecteezy.com/system/resources/previews/002/868/086/original/space-background-illustration-for-explore-in-outer-space-vector.jpg";
+            imgMain.src = "https://64.media.tumblr.com/9129bd384b7e0ecf6fd5d3b9070e2d3e/2dfdaa3dd905833e-d1/s1280x1920/75009ef3c3fbaee6d770259038122e13786294a9.png";
         }
     })
 })
